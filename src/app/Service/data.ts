296acd3e4845c49d217efa0b2b4c80e597271cd9
talha915 <talhazafar915426@gmail.com>
2018-07-15 18:31:39 +0500

    self work

--- src/app/Service/data.ts
+++ src/app/Service/data.ts
@@ -1,25 +1,32 @@
-export interface IData{
-    asset_name: string,
-    longitude: number,
-    latitude: number,
-    performance: string,
-    performance_status: string,
-    alert_level: number
+// export interface IData{
+//     asset_name: string,
+//     longitude: number,
+//     latitude: number,
+//     performance: string,
+//     performance_status: string,
+//     alert_level: number
+// }
+
+// export interface IData{
+//     id: number,
+//     title: string,
+//     releaseYear: number
+// }
+
+export interface IData {
+    id: number,
+    type: string,
+    price: number,
+    address: string,
+    description: string,
+    bedrooms: number,
+    bathrooms: number,
+    area: number,
+    image: string
 }
 
-//   export interface IData{
-//       id: number,
-//       releaseYear: number
-//  }
-
-// export interface IData {
+// export interface IData{
 //     id: number,
-//     type: string,
-//     price: number,
-//     address: string,
-//     description: string,
-//     bedrooms: number,
-//     bathrooms: number,
-//     area: number,
-//     image: string
+//     title: string,
+//     releaseYear: number
 // }
--- src/app/highcharts/highcharts.component.ts
+++ src/app/highcharts/highcharts.component.ts
@@ -16,6 +16,9 @@ export class HighchartsComponent implements OnInit {
   message: any;
   myData: any = [];
   turbine_info;
+  element_id;
+  element_price;
+  element_type;
   constructor(private service_data: DataService) { 
     this.options = {
       chart: {
@@ -38,7 +41,10 @@ export class HighchartsComponent implements OnInit {
           pointStart: 2010
         }
       },
-      series:[]
+      series:[{
+        name: 'MyDar',
+        data: this.Datas
+      }]
         // type: 'area',
         // name: 'Turbine Data',
         // data: data
@@ -50,32 +56,61 @@ export class HighchartsComponent implements OnInit {
   
   ngOnInit() {
     let self =this;
-    
-    this.service_data.getData()
-      .subscribe(function (res) {
+
+    this.service_data.getmyData();
+    console.log("this service", this.service_data.getmyData())
+    const dati = this.service_data.getmyData();
+    console.log("Dati", dati);
+    dati.forEach(function(element){
+      console.log("elements", element);
+      self.element_id = element.id;
+      self.element_price = element.price;
+      self.element_type =element.area;
+      console.log("Id is: ", self.element_id);
+      console.log("Price is: ", self.element_price);
+      
+      var mydata = {
+        x: self.element_id,
+        y: self.element_price,
+        type: self.element_type
+      }
+      self.Datas.push(mydata);
+      console.log("MY DATAS>>>", self.Datas);
+
+      self.options.series[{
+        name: 'Name',
+        data: self.Datas
+      }]
+    })
+   // this.service_data.getData()
+    //  .subscribe(function (res) {
         //self.drawChart(res.turbine_info)
-        this.Data = res.turbine_info;
-        console.log("Response", res.turbine_info[0].latitude, res.turbine_info[0].longitude);
-        console.log("DATAS", this.Data);
-        this.Data.forEach(function(element){
-          console.log("elements", element);
-          element.x = element.latitude,
-          console.log("elements lat", element.x);
-          element.y = element.longitude
-          console.log("elements long", element.y);
-         var mydata={
-           x: element.x,
-           y: element.y
-         }
-         console.log("MyData", mydata);
-         self.Datas.push(mydata)
-         console.log("Self.Datas", self.Datas)
-        })
-        console.log("Self Datas", self.Datas);
-        self.options.series = [{
-          name: 'TUR',
-          data: self.Datas
-        }];
+        //this.Data = res.turbine_info;
+        //console.log("Response", res.turbine_info[0].latitude, res.turbine_info[0].longitude);
+        
+    //    console.log("Response", res);
+
+        // this.Data.forEach(function(element){
+        //   console.log("elements", element);
+        //   element.x = element.latitude,
+        //   console.log("elements lat", element.x);
+        //   element.y = element.longitude
+        //   console.log("elements long", element.y);
+        //  var mydata={
+        //    x: element.x,
+        //    y: element.y
+        //  }
+        //  console.log("MyData", mydata);
+        //  self.Datas.push(mydata)
+        //  console.log("Self.Datas", self.Datas)
+        // })
+        // console.log("Self Datas", self.Datas);
+        // this.options.series = [{
+        //   name: 'TUR',
+        //   data: self.Datas
+        // }];
+
+
         // this.options.series[{
         //   name: 'Graph',
         //   data: data
@@ -93,7 +128,7 @@ export class HighchartsComponent implements OnInit {
         // if(res.id !==null && res.releaseYear!==null){
         //   console.log("res", res.movies[0]);
         // }
-      })
+     // })
       
      
   }
